b68ea90236721077466914e2d514a6d2efc8de4b
arpydanielyan <arpydanielyan2019@gmail.com>
2022-02-05 21:42:04 +0400

    Main

--- src/assets/imeges/index.js
+++ src/assets/imeges/index.js
@@ -2,6 +2,8 @@ export {default as Bitmap} from "./icons/Bitmap.png";
 export {default as Lukas} from "./icons/pexels-lukas.png";
 export {default as Ready} from "./icons/ready-made.png";
 export {default as BitmapWhite} from "./icons/Bitmap-white.png";
+export {default as RoseImg} from "./icons/rose-img.png";
+export {default as RoseArt} from "./icons/rose-art.png";
 
  export {ReactComponent as Man} from "./icons/MAN.svg";
  export {ReactComponent as Search} from "./icons/Search.svg"
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,11 +1,11 @@
-import {Lukas,Ready} from "../../assets/imeges";
+import {Lukas,Ready,RoseImg,RoseArt} from "../../assets/imeges";
 import stayle from "./main.module.scss";
 
 function Main() {
 
     return <div className={stayle.main}>
+        <div className={stayle.product}>
         <div className={stayle.backWhite}>
-            <div className={stayle.product}>
                 <div className={stayle.choos}>
                     <h1>ВЫБЕРИ СВОЕ СЧАСТЬЕ</h1>
                     <p>С нашими опытными разработчиками вы сможете воплотить
@@ -13,19 +13,23 @@ function Main() {
                         все, чтобы ваша идея развивалась…</p>
                     <button>Продукт</button>
                 </div>
-                <img src={Ready} alt=""/>
             </div>
+            <div className={stayle.roseImg}><img src={RoseImg} alt=""/></div>
+             <div className={stayle.rose}><img src={Ready} alt=""/></div>
         </div>
-        <div className={stayle.backRed}>
             <div className={stayle.aboutUs}>
-                <img src={Lukas} alt=""/>
+                <div className={stayle.backRed}>
                 <div className={stayle.more}>
+                    <div className={stayle.roseArt}><img src={Lukas} alt=""/></div>
+                    <div className={stayle.text}>
                     <h1>О нас</h1>
                     <p>Наши опытные разработчики могут воплотить любую вашу идею в реальность:
                         Опытная разработка может воплотить в жизнь все ваши идеи разработчиков и:
                         может воплотить все ваши идеи в muexperie makere все ваши идеи:
                         разработчики могут воплотить в жизнь все ваши идеи в конце проекта.</p>
                     <button>БОЛЬШЕ ></button>
+                    </div>
+                    <div className={stayle.art}><img src={RoseArt} alt=""/></div>
                 </div>
             </div>
         </div>
